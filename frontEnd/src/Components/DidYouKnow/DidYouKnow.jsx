4304e26a3f096818396a96733bc7d640284f1679
MarsIfeanyi <marschalice@gmail.com>
2023-10-28 22:51:27 +0100

    Create DidYouKnow Components

---
+++ frontEnd/src/Components/DidYouKnow/DidYouKnow.jsx
@@ -0,0 +1,13 @@
+import design from "./style.module.css"
+
+ const DidYouKnow = ( ) => {
+  return (
+  <div className={design.DidYouKnow_container}>
+    <h4>Did you know ?</h4>
+    <p>USDC is a faster, safer, and more efficient way to send, spend, and exchange money around the globe. USDC powers apps to provide anytime access to payments and financial services.</p>
+  </div>
+  );
+};
+
+
+export default DidYouKnow;
