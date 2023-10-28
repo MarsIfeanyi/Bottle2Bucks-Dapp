12b02f3a2f86517f82a19187cf191df76f777e99
MarsIfeanyi <marschalice@gmail.com>
2023-10-28 22:42:19 +0100

    Update File

---
+++ frontEnd/contracts/bottleBucksContract.js
@@ -0,0 +1,6 @@
+// import contractABI from "./contract.json";
+import abi from "./abi.json";
+
+export const CONTRACT_ADDRESS = "0x5C2c8C4E10807A9917814714Be3490848305eC04";
+
+export const CONTRACT_ABI = abi;
