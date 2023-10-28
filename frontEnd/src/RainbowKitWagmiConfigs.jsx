d1ead306a12754a92dbaddb836f2433d1858ede5
MarsIfeanyi <marschalice@gmail.com>
2023-10-28 22:48:33 +0100

    Add RainbowkitWagmiConfig

---
+++ frontEnd/src/RainbowKitWagmiConfigs.jsx
@@ -0,0 +1,61 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+  lightTheme,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+const { chains, publicClient } = configureChains(
+  [polygonMumbai],
+  [
+    alchemyProvider({
+
+
+
+
+      apiKey: import.meta.env.VITE_REACT_APP_MUMBAI_API_KEY,
+
+      
+    }),
+    jsonRpcProvider({ rpc: () => ({ http: "https://rpc.ankr.com/gnosis" }) }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "EcoDriveChain dApp",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
+});
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+const RainbowKitWagmiConfigs = ({ children }) => {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        coolMode
+        chains={chains}
+        theme={lightTheme({
+          accentColor: "#8BC34A", //color of wallet  try #703844
+          accentColorForeground: "white", //color of text
+          borderRadius: "large", //rounded edges
+          fontStack: "system",
+        })}
+      >
+        {children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+};
+
+export default RainbowKitWagmiConfigs;
