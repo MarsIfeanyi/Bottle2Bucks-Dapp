a77b12bd5f4958ffffd23a41933f16155136630c
MarsIfeanyi <marschalice@gmail.com>
2023-10-30 01:43:17 +0100

    Update  Component

--- frontEnd/src/userAuthenticate/PrivateRoutes.jsx
+++ frontEnd/src/userAuthenticate/PrivateRoutes.jsx
@@ -1,28 +1,20 @@
+import { useLocation, Navigate } from "react-router-dom";
 
-import {useLocation, Navigate } from 'react-router-dom';
-
-import Dashboard from '../Page/Dashboard/Dashboard';
-import AuthContext from '../context/AuthProvider';
-import { useContext } from 'react';
-
+import Dashboard from "../Page/Dashboard/Dashboard";
+import AuthContext from "../context/AuthProvider";
+import { useContext } from "react";
 
 function PrivateRoutes() {
-
   const { auth } = useContext(AuthContext);
   const { location } = useLocation();
 
-
-  return (
-    auth?.success ?
+  return auth?.success ? (
     <>
-    <Dashboard/>
-    </> 
-     : 
-    
-    
-    <Navigate to='/login' state={{ from : location}} replace/>
-
-  )
+      <Dashboard />
+    </>
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 }
 
-export default PrivateRoutes
+export default PrivateRoutes;
